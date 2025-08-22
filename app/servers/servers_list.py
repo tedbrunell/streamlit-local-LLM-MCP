2e04d5ff4bd813b414eee80c16f2607583460a74
Ted <tbrunell@redhat.com>
2025-08-22 13:09:33 -0400

    add KUBE_FILE_PATH environment variable

--- app/servers/servers_list.py
+++ app/servers/servers_list.py
@@ -6,7 +6,9 @@ from mcp import StdioServerParameters
 server_list = {
 	"g-maps": StdioServerParameters(
 		command="/usr/bin/npx",
-		args=["-y", "@modelcontextprotocol/server-google-maps"],
+		args=[
+		    "-y", "@modelcontextprotocol/server-google-maps"
+		],
 		env={
 			"GOOGLE_MAPS_API_KEY": os.getenv("GMAP_API_KEY")
 		}
@@ -19,10 +21,8 @@ server_list = {
     "kubernetes": StdioServerParameters(
         command="/usr/bin/npx",
         args=[
-            "-y",
-            "kubernetes-mcp-server@latest",
-            "--kubeconfig",
-            "/home/tbrunell/.kube/config",
+            "-y", "kubernetes-mcp-server@latest",
+            "--kubeconfig", os.environ["KUBE_FILE_PATH"]
         ]
     ),
     "github": StdioServerParameters(
